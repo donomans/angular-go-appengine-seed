da51227358b4bf26cf30098728d44f018ceb3e6c
Donovan Sandey <fronn1@gmail.com>
2013-03-13 22:37:15 -0500

    Added new go files
    Added a controllers and models go file

---
+++ goseed/controllers.go
@@ -0,0 +1,14 @@
+package goseed
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	if err := indexTemplate.Execute(w, "Angular Go Seed"); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+var indexTemplate = template.Must(template.New("index.html").ParseFiles("app/index.html"))
--- goseed/init.go
+++ goseed/init.go
@@ -1,18 +1,9 @@
 package goseed
 
 import (
-	"html/template"
 	"net/http"
 )
 
 func init() {
 	http.HandleFunc("/", index)
 }
-
-func index(w http.ResponseWriter, r *http.Request) {
-	if err := indexTemplate.Execute(w, "Angular Go Seed"); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-var indexTemplate = template.Must(template.New("index.html").ParseFiles("app/index.html"))
