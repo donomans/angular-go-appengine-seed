fe9a535f77d884401c43e191cdf6444f17e79119
Donovan Sandey <fronn1@gmail.com>
2013-03-15 20:10:33 -0500

    Added login and robot/sitemap files

--- goseed/controllers.go
+++ goseed/controllers.go
@@ -1,12 +1,24 @@
 package goseed
 
 import (
+	"appengine"
+	"appengine/user"
 	"html/template"
 	"net/http"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	if err := indexTemplate.Execute(w, "Angular Go Seed"); err != nil {
+	context := appengine.NewContext(r)
+	currentuser := user.Current(context)
+	var url string = "/"
+	if currentuser == nil {
+		url, _ = user.LoginURL(context, "/")
+
+	} else {
+		url, _ = user.LogoutURL(context, "/")
+	}
+
+	if err := indexTemplate.Execute(w, struct{ Title, LoginUrl string }{Title: "Angular Go Seed", LoginUrl: url}); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
